da81d44c20d939b97ca8582ddbecdeb170e8acc0
unknown <kevinkristansa@gmail.com>
2025-11-19 14:59:15 +0700

    Update Faktur Lengkap

---
+++ backend/internal/service/service_penjualan.go
@@ -0,0 +1,265 @@
+package service
+
+import (
+	"api-distributor/helper"
+	"api-distributor/internal/dto"
+	"api-distributor/internal/model"
+	"api-distributor/internal/repository"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type ServicePenjualan interface {
+	//GetAllPenjualan() ([]dto.PenjualanResponse, error)
+	CreatePenjualan(penjualan dto.CreatePenjualanRequest) (dto.PenjualanResponse, error)
+}
+
+type servicePenjualan struct {
+	repo          repository.RepositoryPenjualan
+	repoBarang    repository.RepositoryBarang
+	repoKartuStok repository.RepositoryKartuStok
+	repoDetilJual repository.RepositoryPenjualanDetail
+	db            *gorm.DB
+}
+
+func NewServicePenjualan(db *gorm.DB, repo repository.RepositoryPenjualan, repoKartuStok repository.RepositoryKartuStok, repoBarang repository.RepositoryBarang, repoDetilJual repository.RepositoryPenjualanDetail) ServicePenjualan {
+	return &servicePenjualan{
+		repo:          repo,
+		repoBarang:    repoBarang,
+		repoKartuStok: repoKartuStok,
+		repoDetilJual: repoDetilJual,
+		db:            db,
+	}
+}
+
+// func (s *servicePenjualan) GetAllPenjualan() ([]dto.PenjualanResponse, error) {
+// 	penjualan, err := s.repo.GetAllPenjualan()
+// 	if err != nil {
+// 		return []dto.PenjualanResponse{}, err
+// 	}
+
+// 	// convert model to dto
+// 	jualDTO := helper.ConvertToDTOPenjualanPlural(penjualan)
+
+// 	return jualDTO, nil
+// }
+
+func (s *servicePenjualan) CreatePenjualan(req dto.CreatePenjualanRequest) (dto.PenjualanResponse, error) {
+	var response dto.PenjualanResponse // untuk tampung response akhir
+
+	// insert data master dan detil penjualan menggunakan transaction
+	tx := s.db.Begin() // mulai transaction
+
+	if tx.Error != nil {
+		return response, tx.Error // jika terjadi error
+	}
+
+	// parse tanggal dari string (input) ke time.Time
+	tglJual, err := time.Parse("2006-01-02", req.TglPenjualan)
+	if err != nil {
+		return dto.PenjualanResponse{}, err
+	}
+
+	tglJatuhTempo, err := time.Parse("2006-01-02", req.TglJatuhTempo)
+	if err != nil {
+		return dto.PenjualanResponse{}, err
+	}
+
+	// mapping dto ke model penjualan
+	penjualan := model.Penjualan{
+		NoFaktur:      req.NoFaktur,
+		TglPenjualan:  tglJual,
+		TglJatuhTempo: tglJatuhTempo,
+		TokoID:        req.TokoID,
+		Total:         req.Total,
+		TotalNetto:    req.TotalNetto,
+		Keterangan:    req.Keterangan,
+	}
+
+	// insert master penjualan
+	if err := s.repo.CreatePenjualan(tx, &penjualan); err != nil {
+		tx.Rollback() // jika terjadi error, rollback
+		return response, err
+	}
+
+	// insert detail penjualan
+	for _, item := range req.Items {
+		detilJual := model.PenjualanDetail{
+			PenjualanID: penjualan.ID, // auto return id dari hasil insert data master penjualan
+			BarangID:    item.BarangID,
+			Size:        item.Size,
+			Qty:         item.Qty,
+			Harga:       item.Harga,
+			Subtotal:    item.Qty * item.Harga,
+		}
+
+		// insert data detil penjualan
+		if err := s.repo.CreatePenjualanDetail(tx, &detilJual); err != nil {
+			tx.Rollback()
+			return response, err
+		}
+
+		// -- SECTION update stok barang
+		// ****TIDAK DIPAKAI LAGI KARENA UPDATE STOK DARI KARTU STOK ****//
+		/*
+			// validasi apakah barang ada di db
+			barang, err := s.repoBarang.GetBarangById(item.BarangID)
+			if err != nil {
+				tx.Rollback()
+				return dto.PenjualanResponse{}, err
+			}
+
+			// validasi jumlah stok
+			if barang.Stok < item.Qty {
+				tx.Rollback()
+				return dto.PenjualanResponse{}, errors.New("Stok barang tidak cukup (ID:" + barang.Artikel.Nama + ")")
+			}
+
+			// jika stok cukup, kurangi stok barang aktif
+			barang.Stok -= item.Qty
+			if err := s.repoBarang.UpdateStok(tx, barang); err != nil {
+				return dto.PenjualanResponse{}, err
+			}
+		*/
+
+		// untuk get data barang
+		// validasi apakah barang ada di db
+		barang, err := s.repoBarang.GetBarangById(item.BarangID)
+		if err != nil {
+			tx.Rollback()
+			return dto.PenjualanResponse{}, err
+		}
+
+		// -- SECTION update stok barang di kartu stok
+		// validasi apakah barang ada di tabel kartu stok
+		kartuStok, err := s.repoKartuStok.GetStokByBarangIdAndSize(item.BarangID, item.Size)
+		if err != nil {
+			tx.Rollback()
+			return dto.PenjualanResponse{}, err
+		}
+
+		// validasi stok barang di kartu stok
+		if kartuStok.Stok < item.Qty {
+			tx.Rollback()
+			return dto.PenjualanResponse{}, errors.New("Stok barang tidak cukup (ID:" + barang.Artikel.Nama + ")")
+		}
+
+		// jika stok cukup, kurangi stok barang aktif
+		kartuStok.Stok -= item.Qty
+		if err := s.repoKartuStok.UpdateStokDiKartuStok(tx, kartuStok); err != nil {
+			return dto.PenjualanResponse{}, err
+		}
+
+	}
+
+	// commit transaction
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return response, err
+	}
+
+	// convert dulu req detil penjualan ke dalam response detil penjualan
+	itemsDTO := helper.ConvertItemRequestToItemResponse(req.Items)
+
+	// get penjualan by id untuk memperoleh relasi dengan master toko untuk tampilkan nama toko, alamat, dan kotanya
+	newPenjualan, err := s.repo.GetPenjualanById(penjualan.ID)
+	if err != nil {
+		return dto.PenjualanResponse{}, err
+	}
+
+	// mapping model ke response dto
+	response = dto.PenjualanResponse{
+		ID:           penjualan.ID,
+		NoFaktur:     penjualan.NoFaktur,
+		TglPenjualan: penjualan.TglPenjualan.Format("2006-01-02"),
+		TokoID:       penjualan.TokoID,
+		TokoNama:     newPenjualan.Toko.Nama,
+		Total:        penjualan.Total,
+		Keterangan:   penjualan.Keterangan,
+		Items:        itemsDTO,
+		//TokoNama:   penjualan.Toko.Nama,
+	}
+
+	// ==============================
+	// Tambahkan bagian cetak faktur
+	// ==============================
+	invoiceText := s.generateInvoiceText(newPenjualan)
+
+	// Simpan ke file (opsional)
+	fileName := fmt.Sprintf("faktur_%s.txt", penjualan.NoFaktur)
+	if err := os.WriteFile(fileName, []byte(invoiceText), 0644); err != nil {
+		fmt.Println("Gagal menulis faktur:", err)
+	}
+
+	return response, nil
+}
+
+// helper function untuk cetak faktur
+func (s *servicePenjualan) generateInvoiceText(penjualan model.Penjualan) string {
+	var sb strings.Builder
+	total := 0
+
+	sb.WriteString(strings.Repeat("=", 80) + "\n")
+	sb.WriteString(fmt.Sprintf("MITRA SUKSES BERSAMA%10sNama Toko: %s\n", "", penjualan.Toko.Nama))
+	sb.WriteString(fmt.Sprintf("FAKTUR PENJUALAN%13s %s (%s)\n", "", penjualan.Toko.Alamat, penjualan.Toko.Kota.Nama))
+	sb.WriteString(fmt.Sprintf("No.Faktur: %-10s         Tanggal Faktur: %s\n",
+		penjualan.NoFaktur,
+		penjualan.TglPenjualan.Format("02-01-2006")))
+	sb.WriteString(fmt.Sprintf("%30sJatuh Tempo: %s\n", "", penjualan.TglJatuhTempo.Format("02-01-2006")))
+	sb.WriteString(strings.Repeat("=", 80) + "\n")
+	sb.WriteString("| ID / Artikel              | Warna        | Size | Harga    | Qty | Subtotal  |\n")
+	sb.WriteString(strings.Repeat("-", 80) + "\n")
+
+	//ambil data detil penjualan berdasarkan nomor faktur
+	newDetilJual, err := s.repoDetilJual.GetPenjualanDetailById(penjualan.ID)
+	// fmt.Println(newDetilJual)
+	if err != nil {
+		panic(err)
+	}
+
+	detilJualDTO := helper.ConvertToDTOPenjualanDetailPlural(newDetilJual)
+	// fmt.Println(detilJualDTO)
+
+	for _, item := range detilJualDTO {
+		sub := item.Qty * item.Harga
+		total += sub
+		sb.WriteString(fmt.Sprintf("| %-5s/%-18s | %-12s | %4d | %8d | %3d | %9d |\n",
+			item.BarangKode, item.BarangArtikel, item.BarangWarna, item.Size, item.Harga, item.Qty, item.Subtotal))
+	}
+
+	// perhitungan diskon 1,2, dan 3
+	var grandTotal float64
+	grandTotal = float64(total)
+	diskon1 := penjualan.Toko.Disc1 / 100 * grandTotal
+	grandTotal = grandTotal - diskon1
+	diskon2 := penjualan.Toko.Disc2 / 100 * grandTotal
+	grandTotal = grandTotal - diskon2
+	diskon3 := penjualan.Toko.Disc3 / 100 * grandTotal
+	//grandTotal = grandTotal - diskon3
+	sb.WriteString(strings.Repeat("-", 80) + "\n")
+	sb.WriteString(fmt.Sprintf("| %64s | %9d |\n", "Total", total))
+
+	sb.WriteString(fmt.Sprintf("| %64s | %9f |\n", "Disc 1", diskon1))
+	sb.WriteString(fmt.Sprintf("| %64s | %9f |\n", "Disc 2", diskon2))
+	sb.WriteString(fmt.Sprintf("| %64s | %9f |\n", "Disc 3", diskon3))
+
+	sb.WriteString(fmt.Sprintf("| %64s | %9d |\n", "Grand Total", penjualan.TotalNetto))
+	sb.WriteString(strings.Repeat("=", 80) + "\n")
+	sb.WriteString(centerText("TERIMA KASIH ATAS PEMBELIAN ANDA", 80) + "\n")
+	sb.WriteString(strings.Repeat("=", 80) + "\n")
+
+	return sb.String()
+}
+
+func centerText(text string, width int) string {
+	padding := (width - len(text)) / 2
+	if padding < 0 {
+		padding = 0
+	}
+	return strings.Repeat(" ", padding) + text
+}
